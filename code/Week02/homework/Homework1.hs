c04dfc66e3c216b5a978c6d764be4e04d2998637
Michal Cisárik <cisarik@users.noreply.github.com>
2023-03-08 19:59:14 +0100

    Week02 homework all 8 tests passed

--- code/Week02/homework/Homework1.hs
+++ code/Week02/homework/Homework1.hs
@@ -16,12 +16,8 @@ import           Utilities            (wrap)
 {-# INLINABLE mkValidator #-}
 -- This should validate if and only if the two Booleans in the redeemer are True!
 mkValidator :: () -> (Bool, Bool) -> PlutusV2.ScriptContext -> Bool
-mkValidator _ redeemer _ = fst redeemer && snd redeemer
-
--- t e s t s    p a s s e d   w i t h: 
---mkValidator _ (True,True) _ = True
---mkValidator _ (False,False) _ = True  
---mkValidator _ _ _ = False
+mkValidator _ (True,True) _ = True
+mkValidator _ _ _ = False
 
 
 wrappedVal :: BuiltinData -> BuiltinData -> BuiltinData -> ()
